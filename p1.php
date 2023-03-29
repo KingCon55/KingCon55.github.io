524192a4e67c23d88f798cf0d020c4efba42d18b
KingCon55 <5sporksgaming@gmail.com>
2023-03-29 19:06:29 -0400

    Update p1.php

--- p1.php
+++ p1.php
@@ -20,23 +20,23 @@ $database_in_use = "user_db";
 $mysqli = new mysqli($servername, $username, $password, $database_in_use);
 
 //display values in table one at a time
-if ($mysqli->connect_error) {
-		echo "Failed to connect to MySQL: (" . $mysqli->connect_error . ") " . $mysqli->connect_error;
+if ($mysqli->connect_errno) {
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 echo $mysqli->host_info . "<br>";
 
-$sql = "SELECT uname, creationdate, expirationdate, password FROM user_info";
-$result = $mysqli->query($sql);
+//$sql = "SELECT uname, creationdate, expirationdate, password FROM user_info";
+//$result = $mysqli->query($sql);
 
-if ($result->num_rows > 0) {
+//if ($result->num_rows > 0) {
   // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "username: " . $row["uname"]. " creation date: " . $row["creationdate"]. " experation date " . $row["experationdate"]. " Password: " . $row["password"] . "<br>";
-  }
-} else {
-  echo "0 results";
-}
-$mysqli->close();
+  //while($row = $result->fetch_assoc()) {
+    //echo "username: " . $row["uname"]. " creation date: " . $row["creationdate"]. " experation date " . $row["experationdate"]. " Password: " . $row["password"] . "<br>";
+  //}
+//} else {
+  //echo "0 results";
+//}
+//$mysqli->close();
 ?>
 
 </body>
