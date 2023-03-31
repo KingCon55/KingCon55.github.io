5f5590ade0b67d68cb2f6ab67beb138395c4601e
KingCon55 <5sporksgaming@gmail.com>
2023-03-31 13:32:37 -0400

    Update p1.php

--- p1.php
+++ p1.php
@@ -21,7 +21,7 @@ $mysqli = new mysqli($servername, $username, $password, $database_in_use);
 
 //display values in table one at a time
 if ($mysqli->connect_errno) {
-		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 echo $mysqli->host_info . "<br>";
 
